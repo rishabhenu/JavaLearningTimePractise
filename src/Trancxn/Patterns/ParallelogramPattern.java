085da21c4ac8120ed9ea7063ac1a5c47db9d72cf
Rishabh Sharma <45663977+rishabhenu@users.noreply.github.com>
2019-07-23 22:24:22 +0530

    Create ParallelogramPattern.java

---
+++ src/Trancxn/Patterns/ParallelogramPattern.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ * @author Rishabh Sharma
+ * 
+ * @desc print a pattern like this
+ * 					@
+ * 				@		@
+ * 			@		@		@
+ * 				@		@
+ * 					@
+ *
+ */
+
+public class ParallelogramPattern {
+
+	public static void main(String... strings) {
+		int n = 4;
+		for (int i = 1; i <= n; i++) {
+			for (int k = 1; k <= n - i; k++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <= i; j++) {
+				System.out.print("@ ");
+			}
+			System.out.println();
+		}
+		
+		for(int i=1;i<n;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print(" ");
+			}
+			for(int j=n-i;j>0;j--) {
+				System.out.print("@ ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
